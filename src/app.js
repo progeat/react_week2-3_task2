1a949b5b3f0ebfc62a7fe2a30858fd4714020a98
Хохлов Андрей Анатольевич <A.Hohlov@transtelematica.ru>
2024-07-02 16:42:23 +0300

    modified file app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,9 @@ import data from './data.json';
 export const App = () => {
 	const [steps, setSteps] = useState(data);
 	const [activeIndex, setActiveIndex] = useState(0);
-
-	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
-	const [isStepFirst, setIsStepFirst] = useState(false);
+	const [isStepFirst, setIsStepFirst] = useState(true);
 	const [isStepLast, setIsStepLast] = useState(false);
 
-	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 	const onClickStepBack = () => {
 		setActiveIndex((updateActiveIndex) => --updateActiveIndex);
 		setIsStepFirst(!(activeIndex - 1));
@@ -63,33 +60,6 @@ export const App = () => {
 								{item.title}
 							</li>
 						))}
-						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
-						{/* <li className={styles['steps-item'] + ' ' + styles.done}> */}
-						{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-						{/* <button className={styles['steps-item-button']}>1</button> */}
-						{/* При клике на кнопку установка выбранного шага в качестве активного */}
-						{/* Шаг 1
-						</li>
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							<button className={styles['steps-item-button']}>2</button>
-							Шаг 2
-						</li>
-						<li
-							className={
-								styles['steps-item'] +
-								' ' +
-								styles.done +
-								' ' +
-								styles.active
-							}
-						>
-							<button className={styles['steps-item-button']}>3</button>
-							Шаг 3
-						</li>
-						<li className={styles['steps-item']}>
-							<button className={styles['steps-item-button']}>4</button>
-							Шаг 4
-						</li> */}
 					</ul>
 					<div className={styles['buttons-container']}>
 						<button
@@ -104,8 +74,6 @@ export const App = () => {
 							onClick={!isStepLast ? onClickStepNext : onClickStepsReset}
 						>
 							{!isStepLast ? 'Далее' : 'Начать сначала'}
-							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-							{/* Или заменять всю кнопку в зависимости от условия */}
 						</button>
 					</div>
 				</div>
